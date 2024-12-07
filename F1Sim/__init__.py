eeef0ea37bfaa5055ddbc7361bead783d0cffb71
MarcoTondolo <tondolo.marco.bt@gmail.com>
2024-12-07 12:13:21 +0100

    flet

--- F1Sim/__init__.py
+++ F1Sim/__init__.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    # Registrazione dei blueprint
-    from F1Sim.lineup import lineup_blueprint  # Importa il blueprint
-    app.register_blueprint(lineup_blueprint)
-
-    # Altre inizializzazioni (es: database, estensioni)
-    # init_db(app)  # Esempio: inizializzazione database
-
-    return app
+from .app import app
