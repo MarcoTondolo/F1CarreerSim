8817e1469923c03000f1c469dc92f5c25a0cc042
MarcoTondolo <tondolo.marco.bt@gmail.com>
2025-08-07 15:24:35 +0200

    minor fix

--- F1Sim/app.py
+++ F1Sim/app.py
@@ -68,6 +68,7 @@ def inizializza_simulazione(nome_giocatore, nome_team):
     giocatore.race_wins = 0
     giocatore.wdc = []
     giocatore.wcc = []
+    giocatore.rating = 50
     team_iniziale.piloti.append(giocatore)
     team_iniziale.piloti.remove(pilota_sostituito)
     piloti.append(giocatore)
